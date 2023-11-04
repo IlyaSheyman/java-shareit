01bf053999d074e995473ce263e5fe94df9a6687
Ilya Sheyman <ilyasheiman@yandex.ru>
2023-11-04 20:07:04 +0300

    fix: еще один коммит для проверок в github

---
+++ src/main/java/ru/practicum/shareit/user/storage/UserStorage.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit.user.storage;
+
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
+
+public interface UserStorage {
+    User add(User user);
+
+    User update(int id, User user);
+
+    User delete(int id);
+
+    User get(int id);
+    List<User> getAll();
+
+
+
+
+}
