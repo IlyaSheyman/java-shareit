3053c8fd2d7652a5db1f230eb3fa5cfc6bacdcb5
Ilya Sheyman <ilyasheiman@yandex.ru>
2023-12-05 12:22:20 +0300

    feature: начало работы в ветке add-requests

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,35 @@
 package ru.practicum.shareit.request;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import javax.validation.Valid;
+
 @RestController
+@Slf4j
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
+
+    ItemRequestService itemRequestService;
+
+    public ItemRequestController(@Qualifier("ItemRequestServiceDbImpl") ItemRequestService itemRequestService) {
+        this.itemRequestService = itemRequestService;
+    }
+
+    @ResponseBody
+    @PostMapping
+    public ItemRequestDto addItemRequest(@Valid @RequestBody ItemRequestDto request,
+                                  @RequestHeader("X-Sharer-User-Id") int userId) {
+        log.info("Получен запрос на добавление вещи.");
+        return null;
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/request/ItemRequestServiceDbImpl.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.request;
+
+import org.springframework.stereotype.Service;
+
+@Service("ItemRequestServiceDbImpl")
+public class ItemRequestServiceDbImpl implements ItemRequestService {
+}
